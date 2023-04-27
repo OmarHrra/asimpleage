af71ca85c02c8c470b6369501c3715589eccf69f
Omar Herrera <omar.hrra@gmail.com>
2023-04-26 21:29:57 -0600

    Create Debug class

--- lib/engine.rb
+++ lib/engine.rb
@@ -1,4 +1,5 @@
 module Engine
   require_relative 'engine/game_manager'
   require_relative 'engine/options'
+  require_relative 'engine/debug'
 end
--- lib/engine/game_manager.rb
+++ lib/engine/game_manager.rb
@@ -1,10 +1,12 @@
 class Engine::GameManager
-  attr_accessor :ui_objects, :game_objects, :options, :world, :camera, :ui, :players
+  attr_accessor :ui_objects, :game_objects, :options, :world, :camera, :ui, :players,
+                :debug
 
   def initialize
     @ui_objects = []
     @game_objects = []
 
     @options = Engine::Options.new self
+    @debug = Engine::Debug.new self if @options.debug
   end
 end
