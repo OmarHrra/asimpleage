475a8a77d362f19227f04630a92a0119584934e9
Omar Herrera <omar.hrra@gmail.com>
2023-04-26 22:08:25 -0600

    Create module MouseTriggerable

--- lib/engine.rb
+++ lib/engine.rb
@@ -4,4 +4,5 @@ module Engine
   require_relative 'engine/debug'
   require_relative 'engine/world'
   require_relative 'engine/camera'
+  require_relative 'engine/mouse_triggerable'
 end
---
+++ lib/engine/mouse_triggerable.rb
@@ -0,0 +1,37 @@
+module MouseTriggerable
+  attr_accessor :click, :hover, :mouse_x, :mouse_y
+
+  def mouse_up(event)
+    mouse_position(event)
+
+    @click = false
+    # puts 'mouse_up'
+    # puts "click: #{@click}"
+  end
+
+  def mouse_down(event)
+    mouse_position(event)
+
+    @click = true if mouse_hover?
+    # puts 'mouse_down'
+    # puts "click: #{@click}"
+  end
+
+  def mouse_move(event)
+    mouse_position(event)
+
+    mouse_hover?
+    # puts "mouse_move hover: #{@hover}"
+  end
+
+  private
+
+  def mouse_hover?
+    @hover = @object.contains?(@mouse_x, @mouse_y)
+  end
+
+  def mouse_position(event)
+    @mouse_x = event.x
+    @mouse_y = event.y
+  end
+end
