98ca8decbab5cc727dd78efea2be0b88f78aae73
Omar Herrera <omar.hrra@gmail.com>
2023-05-01 19:40:46 -0600

    Add attribute always_visible

--- lib/engine/game_object.rb
+++ lib/engine/game_object.rb
@@ -1,6 +1,6 @@
 class Engine::GameObject
   attr_accessor :game_manager, :type, :object, :path, :x, :y, :width, :height, :color, :rotate, :z,
-                :fixed_at_camera, :visible
+                :opacity, :fixed_at_camera, :visible, :always_visible
 
   def initialize(game_manager, **attrs)
     @game_manager = game_manager
@@ -13,6 +13,7 @@ class Engine::GameObject
 
     @fixed_at_camera = attrs[:fixed_at_camera] || false
     @visible = true
+    @always_visible = attrs[:always_visible] || false
   end
 
   def update
@@ -34,6 +35,7 @@ class Engine::GameObject
   private
 
   def define_attriutes(attrs = nil)
+    # Image
     @path = @object&.path || attrs[:path]
     @x = @object&.x || attrs[:x]
     @y = @object&.y || attrs[:y]
@@ -41,6 +43,7 @@ class Engine::GameObject
     @height = @object&.height || attrs[:height]
     @color = @object&.color || attrs[:color]
     @rotate = @object&.rotate || attrs[:rotate]
+    @opacity = attrs[:opacity] || 1 if not attrs.nil?
     @z = @object&.z || attrs[:z]
   end
 
@@ -54,6 +57,7 @@ class Engine::GameObject
         height: @height,
         color: @color,
         rotate: @rotate,
+        opacity: @opacity,
         z: @z
       }.reject { |key, value| value.nil? }
     else
@@ -73,6 +77,8 @@ class Engine::GameObject
   end
 
   def out_of_camera
+    return if @always_visible
+
     if not @object.x >= -(@game_manager.world.tile_width * @game_manager.camera.scale) && @object.x <= @game_manager.options.resolution[:x] && @object.y >= -(@game_manager.world.tile_height * @game_manager.camera.scale) && @object.y <= @game_manager.options.resolution[:y]
       @object.remove
       @visible = false
